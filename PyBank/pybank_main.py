fd402d29986e35b604adbaac12335a987dcb783b
Lori Ta <lori@Loris-MacBook-Air.local>
2019-06-08 20:42:08 -0700

    Calculate average change

--- PyBank/pybank_main.py
+++ PyBank/pybank_main.py
@@ -1,6 +1,6 @@
 # The average of the changes in "Profit/Losses"
-# Greatest increase in profits (date and amount)
-# Greatest decrease in losses (date and amount)
+# Greatest increase in profits (date and amount) (sort)
+# Greatest decrease in losses (date and amount) (sort)
 # Final script should both print the analysis to the terminal and export a text file with the results.
 
 import os
@@ -16,12 +16,14 @@ with open (csvpath, newline='') as csvfile:
 
     header = next(csvreader)
 
-# Count total months
     for row in csvreader:
-        month_count = month_count + 1
+        month_count = month_count + 1 # Count months
+        net_total = net_total + int(row[1]) # Net total
 
-# Net total of "Profit/Losses   
-        net_total = net_total + int(row[1])
-   
-    print(month_count)
-    print(net_total)
+#sort, index 0 and -1, dictionary
+    
+print(f"Total Months: {month_count}")
+print(f"Total: ${net_total}")
+print(f"Average Change: ${round(net_total/month_count, 2)}") # Average
+print(f"Greatest Increase in Proftis: ")
+print(f"Greatest Decrease in Proftis: ")

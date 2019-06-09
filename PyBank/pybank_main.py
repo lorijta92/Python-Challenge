e243b291d9a080dde8fc3b001a771f73b3e01a88
Lori Ta <lori@Loris-MacBook-Air.local>
2019-06-08 19:02:30 -0700

    calculate net total

--- PyBank/pybank_main.py
+++ PyBank/pybank_main.py
@@ -1,16 +1,14 @@
-# Total months
-# Net total of "Profit/Losses"
 # The average of the changes in "Profit/Losses"
 # Greatest increase in profits (date and amount)
 # Greatest decrease in losses (date and amount)
 # Final script should both print the analysis to the terminal and export a text file with the results.
 
-# Read in csv, skip header, COUNT TOTAL MONTHS
 import os
 import csv
 
 csvpath = os.path.join("budget_data.csv")
 month_count = 0
+net_total = 0
 
 with open (csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -18,7 +16,12 @@ with open (csvpath, newline='') as csvfile:
 
     header = next(csvreader)
 
+# Count total months
     for row in csvreader:
         month_count = month_count + 1
-    
+
+# Net total of "Profit/Losses   
+        net_total = net_total + int(row[1])
+   
     print(month_count)
+    print(net_total)

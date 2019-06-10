e60fc9b4c5faced63d81b2b78ae6818aca33e452
Lori Ta <lori@Loris-MacBook-Air.local>
2019-06-09 17:05:10 -0700

    Identify greatest increase/decrease and corresponding dates

--- PyBank/pybank_main.py
+++ PyBank/pybank_main.py
@@ -9,7 +9,6 @@ net_total = []
 
 with open (csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
 
     header = next(csvreader)
 
@@ -17,12 +16,14 @@ with open (csvpath, newline='') as csvfile:
         months.append(row[0]) # Count months
         net_total.append(int(row[1])) # Net total
 
-#sort, index 0 and -1, dictionary
-#iterate through rows to zip them as a dictionary?
-#unzip then sort
+max = max(net_total)
+max_index = net_total.index(max)
+
+min = min(net_total)
+min_index = net_total.index(min)
 
 print(f"Total Months: {len(months)}")
 print(f"Total: ${sum(net_total)}")
-print(f"Average Change: ${round(sum(net_total)/len(months), 2)}") # Average
-#print(f"Greatest Increase in Proftis: Date: Amount")
-#print(f"Greatest Decrease in Proftis: Date: Amount")
+print(f"Average Change: ${round(sum(net_total)/len(months), 2)}")
+print(f"Greatest Increase in Profits: {months[max_index]} $({max})")
+print(f"Greatest Decrease in Profits: {months[min_index]} $({min})")
